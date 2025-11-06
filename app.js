0e93588b1f0fbf4bae60a23ead528d97327c586e
itsaot <calvinmzoboshe@gmail.com>
2025-11-06 14:07:29 +0200

    Cors

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH'); // include PATCH if used
     res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
   }
