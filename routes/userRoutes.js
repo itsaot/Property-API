e27bcf425c8921aeffbc653c31df66d996efc100
itsaot <calvinmzoboshe@gmail.com>
2026-03-09 01:43:47 +0200

    Still

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,12 +2,12 @@ const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/upload");
 const { getUserProfile, uploadProfilePicture, updateProfile, searchUsers, updateSettings } = require("../controllers/userController");
-const { auth } = require("../middleware/auth");
+const { protect } = require("../middleware/auth");
 
-router.get("/search", auth, searchUsers);
-router.get("/:id", auth, getUserProfile);
-router.put("/:id", auth, updateProfile);
-router.put("/:id/settings", auth, updateSettings);
+router.get("/search", protect, searchUsers);
+router.get("/:id", protect, getUserProfile);
+router.put("/:id", protect, updateProfile);
+router.put("/:id/settings", protect, updateSettings);
 router.post("/upload-profile", upload.single("image"), uploadProfilePicture);
 
 module.exports = router;
