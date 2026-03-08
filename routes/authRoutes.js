5aa316f215d0d039c473cb4b871ad8a4337ddbcf
itsaot <calvinmzoboshe@gmail.com>
2026-03-09 01:38:37 +0200

    Still at it

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login,logout, getMe, profileSetup, forgotPassword, resetPassword, adminLogin } = require("../controllers/authController");
-const { auth } = require("../middleware/auth");
+const { signup, login, logout, getMe, profileSetup, forgotPassword, resetPassword, adminLogin } = require("../controllers/authController");
+const { protect } = require("../middleware/auth");
 const upload = require("../utils/fileUpload");
 
 const authController = require("../controllers/authController");
@@ -9,9 +9,9 @@ console.log("AUTH CONTROLLER:", authController);
 
 router.post("/signup", upload.single("idDocument"), signup);
 router.post("/login", login);
-router.post("/logout", auth, logout);
-router.get("/me", auth, getMe);
-router.put("/setup", auth, profileSetup);
+router.post("/logout", protect, logout);
+router.get("/me", protect, getMe);
+router.put("/setup", protect, profileSetup);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password/:token", resetPassword);
 
